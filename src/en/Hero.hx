a3d6dfee48211df7a14a143d115337b0464e74c3
Kino  Rose <kinoarose@gmail.com>
2021-09-25 06:38:21 -0400

    Add in a blinking effect when the character gets hit

--- src/en/Hero.hx
+++ src/en/Hero.hx
@@ -11,365 +11,413 @@ import dn.heaps.Controller.ControllerAccess;
  * are entered.
  */
 class Hero extends Entity {
-	public var camera(get, never):Camera;
+  public var camera(get, never):Camera;
 
-	inline function get_camera()
-		return game.camera;
+  inline function get_camera()
+    return game.camera;
 
-	var ct:ControllerAccess;
+  var ct:ControllerAccess;
 
-	public var isOnFloor:Bool;
-	public var canJump:Bool;
-	public var jumpCount:Int = 0;
-	public var attackUnlock:Bool;
-	public var isDashing(get, never):Bool;
-	public var keys:Int = 0;
-	public var health:Int = 3;
-	public var plat:MovingPlatform;
+  public var isOnFloor:Bool;
+  public var canJump:Bool;
+  public var jumpCount:Int = 0;
+  public var attackUnlock:Bool;
+  public var isDashing(get, never):Bool;
+  public var keys:Int = 0;
+  public var health:Int = 3;
+  public var plat:MovingPlatform;
+  public var isInvincible:Bool;
 
-	public static inline var DASH_FORCE:Float = 1.2;
-	public static inline var DASH_TIME:Float = 1.5;
-	public static inline var MAX_SPEED:Float = 0.1;
-	public static inline var HEALTH_CAP:Int = 3;
+  public static inline var DASH_FORCE:Float = 1.2;
+  public static inline var DASH_TIME:Float = 1.5;
+  public static inline var MAX_SPEED:Float = 0.1;
+  public static inline var HEALTH_CAP:Int = 3;
+  public static inline var INVINCIBLE_TIME:Float = 1.5;
 
-	public var dashDir:Vec2 = new Vec2(0, 0);
+  public var dashDir:Vec2 = new Vec2(0, 0);
 
-	inline function get_isDashing() {
-		return dashTimer > 0;
-	}
+  inline function get_isDashing() {
+    return dashTimer > 0;
+  }
 
-	public var dashCount:Int = 0;
-	public var dashTimer:Float;
+  public var dashCount:Int = 0;
+  public var dashTimer:Float;
 
-	public var dashUnlock:Bool;
-	public var doubleJumpUnlock:Bool;
-	public var canDoubleJump(get, never):Bool;
+  public var dashUnlock:Bool;
+  public var doubleJumpUnlock:Bool;
+  public var canDoubleJump(get, never):Bool;
 
-	inline function get_canDoubleJump() {
-		return jumpCount < 2;
-	}
+  inline function get_canDoubleJump() {
+    return jumpCount < 2;
+  }
 
-	public var canGlide:Bool;
-	public var canFly:Bool;
+  public var canGlide:Bool;
+  public var canFly:Bool;
 
-	public function new(x:Int, y:Int) {
-		super(x, y);
-		// Debug drags entity location, therefore in the final build we'd shift sprite position up
-		// To offset the position in the game to make it more natural
-		// Assuming platformer 0.5 would be the halfway point for the feet so offset by half sprite size
-		spr.filter = new PixelOutline(0xff70af, 1);
-		var g = new h2d.Graphics(spr);
-		g.beginFill(0xffffff);
-		g.drawRect(0, 0, 16, 16);
-		g.y -= Const.GRID * 0.5;
+  public function new(x:Int, y:Int) {
+    super(x, y);
+    // Debug drags entity location, therefore in the final build we'd shift sprite position up
+    // To offset the position in the game to make it more natural
+    // Assuming platformer 0.5 would be the halfway point for the feet so offset by half sprite size
+    spr.filter = new PixelOutline(0xff70af, 1);
+    var g = new h2d.Graphics(spr);
+    g.beginFill(0xffffff);
+    g.drawRect(0, 0, 16, 16);
+    g.y -= Const.GRID * 0.5;
 
-		isOnFloor = false;
-		canJump = false;
-		#if debug
-		doubleJumpUnlock = true;
-		dashUnlock = true;
-		#else
-		doubleJumpUnlock = false;
-		dashUnlock = false;
-		#end
-		// Add in pixel outline shader
+    isOnFloor = false;
+    canJump = false;
+    #if debug
+    doubleJumpUnlock = true;
+    dashUnlock = true;
+    #else
+    doubleJumpUnlock = false;
+    dashUnlock = false;
+    #end
+    // Add in pixel outline shader
 
-		ct = Main.ME.controller.createAccess('hero');
-		camera.trackEntity(this, true);
-	}
+    ct = Main.ME.controller.createAccess('hero');
+    camera.trackEntity(this, true);
+  }
 
-	override function dispose() {
-		super.dispose();
-		ct.dispose();
-	}
+  override function dispose() {
+    super.dispose();
+    ct.dispose();
+  }
 
-	override function update() {
-		if (ct.leftDown() || ct.isKeyboardDown(K.LEFT)) {
-			if (!isDashing) {
-				dx = M.fclamp((dx - (0.1 * tmod)), -MAX_SPEED, MAX_SPEED);
-			}
+  override function update() {
+    updateInvincible();
+    updateControls();
+    super.update();
+  }
 
-			dashDir.x = -1;
-		}
+  public function updateInvincible() {
+    // Flash Character every couple frames
+    if (isInvincible) {
+      // spr.alpha = 1;
+      if (!cd.has('invincible')) {
+        cd.setF('invincible', 5, () -> {
+          spr.alpha = 0;
+        });
+      } else {
+        spr.alpha = 1;
+      }
+    } else {
+      spr.alpha = 1;
+    }
+  }
 
-		if (ct.rightDown() || ct.isKeyboardDown(K.RIGHT)) {
-			if (!isDashing) {
-				dx = M.fclamp((dx + (0.1 * tmod)), -MAX_SPEED, MAX_SPEED);
-			}
-			dashDir.x = 1;
-		}
+  public function updateControls() {
+    if (ct.leftDown() || ct.isKeyboardDown(K.LEFT)) {
+      if (!isDashing) {
+        dx = M.fclamp((dx - (0.1 * tmod)), -MAX_SPEED, MAX_SPEED);
+      }
 
-		if (ct.upPressed() || ct.isAnyKeyPressed([K.UP, K.W]) && (canJump || (canDoubleJump && doubleJumpUnlock))) {
-			jump();
-		}
+      dashDir.x = -1;
+    }
 
-		// Attack
-		if (ct.aPressed() || ct.isAnyKeyPressed([K.K, K.X])) {
-			attack();
-		}
+    if (ct.rightDown() || ct.isKeyboardDown(K.RIGHT)) {
+      if (!isDashing) {
+        dx = M.fclamp((dx + (0.1 * tmod)), -MAX_SPEED, MAX_SPEED);
+      }
+      dashDir.x = 1;
+    }
 
-		if (ct.bPressed() || ct.isAnyKeyPressed([K.Z, K.J]) && dashUnlock && dashCount > 0) {
-			if (ct.upDown() || ct.isAnyKeyDown([K.UP, K.W])) {
-				dashDir.y = -1;
-			}
-			if (ct.downDown() || ct.isAnyKeyDown([K.DOWN, K.S])) {
-				dashDir.y = 1;
-			}
-			dash();
-		}
-		dashDir.x = 0;
-		dashDir.y = 0;
-		super.update();
-	}
+    if (ct.upPressed() || ct.isAnyKeyPressed([K.UP, K.W])
+      && (canJump || (canDoubleJump && doubleJumpUnlock))) {
+      jump();
+    }
 
-	/**
-	 * Registers a collision in the next cell over based on enemies in the level
-	 */
-	public function attack() {
-		// Based off player direction and what not
-		// aka DashDir
-		if (level.hasAnyEnemyCollision(cx + M.round(dashDir.x), cy)) {
-			var enemy = level.enemyCollided(cx + M.round(dashDir.x), cy);
-			// Take enemy health
-			enemy.takeDamage();
-		}
-	}
+    // Attack
+    if (ct.aPressed() || ct.isAnyKeyPressed([K.K, K.X])) {
+      attack();
+    }
 
-	public function jump() {
-		detachPlat();
-		isOnFloor = false;
-		jumpCount++;
-		dy = 0;
-		dy = (-0.7 * tmod);
-	}
+    if (ct.bPressed()
+      || ct.isAnyKeyPressed([K.Z, K.J])
+      && dashUnlock
+      && dashCount > 0) {
+      if (ct.upDown() || ct.isAnyKeyDown([K.UP, K.W])) {
+        dashDir.y = -1;
+      }
+      if (ct.downDown() || ct.isAnyKeyDown([K.DOWN, K.S])) {
+        dashDir.y = 1;
+      }
+      dash();
+    }
+    dashDir.x = 0;
+    dashDir.y = 0;
+  }
 
-	public function bounce() {
-		isOnFloor = false;
-		dy = 0;
-		dy = (-1.2 * tmod);
-	}
+  /**
+   * Registers a collision in the next cell over based on enemies in the level
+   */
+  public function attack() {
+    // Based off player direction and what not
+    // aka DashDir
+    if (level.hasAnyEnemyCollision(cx + M.round(dashDir.x), cy)) {
+      var enemy = level.enemyCollided(cx + M.round(dashDir.x), cy);
+      // Take enemy health
+      enemy.takeDamage();
+    }
+  }
 
-	/**
-	 * Dashing mechanic allowing the player to dash around the screen
-	 */
-	public function dash() {
-		dashTimer = DASH_TIME;
+  public function jump() {
+    detachPlat();
+    isOnFloor = false;
+    jumpCount++;
+    dy = 0;
+    dy = (-0.7 * tmod);
+  }
 
-		if (dashDir.x == 0 && dashDir.y == 0) {
-			dashDir.x = 1;
-		}
+  public function bounce() {
+    isOnFloor = false;
+    dy = 0;
+    dy = (-1.2 * tmod);
+  }
 
-		// trace('${dashDir.x}, ${dashDir.y}');
-		dx = ((dashDir.x * DASH_FORCE)) * tmod;
-		dy = 0;
-		dy = ((dashDir.y * DASH_FORCE)) * tmod;
+  /**
+   * Dashing mechanic allowing the player to dash around the screen
+   */
+  public function dash() {
+    dashTimer = DASH_TIME;
 
-		dashCount = 0;
-	}
+    if (dashDir.x == 0 && dashDir.y == 0) {
+      dashDir.x = 1;
+    }
 
-	public function takeDamage(value:Int) {
-		health = M.iclamp(health - value, 0, M.T_INT32_MAX);
-		// Screen Shake
-		Game.ME.camera.shakeS(0.5, 0.5);
-		hxd.Res.sound.hit_sfx.play();
-		if (health == 0) {
-			// Destroy / Kill the Object
-			die();
-		}
-	}
+    // trace('${dashDir.x}, ${dashDir.y}');
+    dx = ((dashDir.x * DASH_FORCE)) * tmod;
+    dy = 0;
+    dy = ((dashDir.y * DASH_FORCE)) * tmod;
 
-	public function die() {
-		this.destroy();
-	}
+    dashCount = 0;
+  }
 
-	override function fixedUpdate() {
-		handleCollisions();
-		super.fixedUpdate();
-	}
+  public function takeDamage(value:Int) {
+    if (!isInvincible) {
+      health = M.iclamp(health - value, 0, M.T_INT32_MAX);
+      // Screen Shake
+      Game.ME.camera.shakeS(0.5, 0.5);
+      hxd.Res.sound.hit_sfx.play();
+      if (health == 0) {
+        // Destroy / Kill the Object
+        die();
+      }
+      isInvincible = true;
+      cd.setS('invincibleTime', INVINCIBLE_TIME, () -> {
+        isInvincible = false;
+      });
+    }
+  }
 
-	public function handleCollisions() {
-		levelCollisions();
-		entityCollisions();
-	}
+  public function die() {
+    this.destroy();
+  }
 
-	public function entityCollisions() {
-		collectibleCollisions();
-		hazardCollisions();
-		checkpointCollisions();
-	}
+  override function fixedUpdate() {
+    handleCollisions();
+    super.fixedUpdate();
+  }
 
-	public function checkpointCollisions() {
-		if (level.hasAnyCheckpointCollision(cx, cy)) {
-			var checkpoint = level.checkpointCollided(cx, cy);
-			// var collectibleType = Type.getClass(collectible);
-			// Set the current active checkpoint
-			level.currentCheckpoint = checkpoint;
-			#if debug
-			trace('Touched checkpoint');
-			#end
-		}
-	}
+  public function handleCollisions() {
+    levelCollisions();
+    entityCollisions();
+  }
 
-	public function hazardCollisions() {
-		if (level.hasAnyHazardCollision(cx, cy)) {
-			var hazard = level.collidedHazard(cx, cy);
-			var hazardType = Type.getClass(hazard);
-			switch (hazardType) {
-				case en.hazard.Exit:
-					// Start new level
-					var exit:Exit = cast hazard;
-					game.nextLevel(exit.lvlId);
-				case en.hazard.BouncePad:
-					// Apply speed in the y axis
-					bounce();
-				case en.hazard.Lantern:
-					// Reset dash on touch
-					dashReset();
-				case _:
-					// Do nothing
-			}
-		}
+  public function entityCollisions() {
+    collectibleCollisions();
+    hazardCollisions();
+    checkpointCollisions();
+    enemyCollisions();
+  }
 
-		movingPlatformCollision();
-	}
+  public function checkpointCollisions() {
+    if (level.hasAnyCheckpointCollision(cx, cy)) {
+      var checkpoint = level.checkpointCollided(cx, cy);
+      // var collectibleType = Type.getClass(collectible);
+      // Set the current active checkpoint
+      level.currentCheckpoint = checkpoint;
+      #if debug
+      trace('Touched checkpoint');
+      #end
+    }
+  }
 
-	/**
-	 * Resets the dash and pops the player up slightly
-	 */
-	public function dashReset() {
-		dashCount = 1;
-		dy += -(0.35 * tmod);
-	}
+  public function hazardCollisions() {
+    if (level.hasAnyHazardCollision(cx, cy)) {
+      var hazard = level.collidedHazard(cx, cy);
+      var hazardType = Type.getClass(hazard);
+      switch (hazardType) {
+        case en.hazard.Exit:
+          // Start new level
+          var exit:Exit = cast hazard;
+          game.nextLevel(exit.lvlId);
+        case en.hazard.BouncePad:
+          // Apply speed in the y axis
+          bounce();
+        case en.hazard.Lantern:
+          // Reset dash on touch
+          dashReset();
+        case _:
+          // Do nothing
+      }
+    }
 
-	public function collectibleCollisions() {
-		if (level.hasAnyCollectibleCollision(cx, cy)) {
-			var collectible = level.collidedCollectible(cx, cy);
-			var collectibleType = Type.getClass(collectible);
-			switch (collectibleType) {
-				case en.collectibles.Heart:
-					// Restore player health by 1
-					health = M.iclamp(health + 1, 0, HEALTH_CAP);
-				case en.collectibles.WingBeat:
-					attackUnlock = true;
+    movingPlatformCollision();
+  }
 
-				case en.collectibles.ViridescentWings:
-					dashUnlock = true;
-				case en.collectibles.Key:
-					keys += 1;
+  /**
+   * Resets the dash and pops the player up slightly
+   */
+  public function dashReset() {
+    dashCount = 1;
+    dy += -(0.35 * tmod);
+  }
 
-				case _:
-					// do nothing
-			}
-			collectible.destroy();
-		}
-	}
+  public function enemyCollisions() {
+    if (level.hasAnyEnemyCollision(cx, cy)) {
+      // Take damage when the player touches an enemy in game
+      takeDamage(1);
+    }
+  }
 
-	public function movingPlatformCollision() {
-		// Left
-		if (level.hasAnyMPlatCollision(cx - 1, cy) && xr <= 0.3) {
-			xr = 0.3;
-			dx = 0;
-			// dx = M.fabs(dx);
-		}
+  public function collectibleCollisions() {
+    if (level.hasAnyCollectibleCollision(cx, cy)) {
+      var collectible = level.collidedCollectible(cx, cy);
+      var collectibleType = Type.getClass(collectible);
+      switch (collectibleType) {
+        case en.collectibles.Heart:
+          // Restore player health by 1
+          health = M.iclamp(health + 1, 0, HEALTH_CAP);
+        case en.collectibles.WingBeat:
+          attackUnlock = true;
 
-		// Right
-		if (level.hasAnyMPlatCollision(cx + 1, cy) && xr >= 0.1) {
-			// push back to previous cell
-			xr = 0.1;
-			dx = 0;
-			// dx = (-1 * M.fabs(dx));
-		}
+        case en.collectibles.ViridescentWings:
+          dashUnlock = true;
+        case en.collectibles.Key:
+          keys += 1;
 
-		// Up
-		if (level.hasAnyMPlatCollision(cx, cy - 1) || level.hasAnyMPlatCollision(cx + M.round(xr), cy - 1)) {
-			// Set some squash for when you touch the ceiling
+        case _:
+          // do nothing
+      }
+      collectible.destroy();
+    }
+  }
 
-			// setSquashY(0.8);
-			dy = M.fabs(dy);
-		}
+  public function movingPlatformCollision() {
+    // Left
+    if (level.hasAnyMPlatCollision(cx - 1, cy) && xr <= 0.3) {
+      xr = 0.3;
+      dx = 0;
+      // dx = M.fabs(dx);
+    }
 
-		// Down
-		if (level.hasAnyMPlatCollision(cx, cy + 1) && yr >= 0. || level.hasAnyMPlatCollision(cx + M.round(xr), cy + 1) && yr >= 0.5) {
-			// Handle squash and stretch for entities in the game
+    // Right
+    if (level.hasAnyMPlatCollision(cx + 1, cy) && xr >= 0.1) {
+      // push back to previous cell
+      xr = 0.1;
+      dx = 0;
+      // dx = (-1 * M.fabs(dx));
+    }
 
-			var mPlat:MovingPlatform = cast level.collidedMPlat(cx, cy + 1);
-			isOnFloor = true;
-			canJump = true;
+    // Up
+    if (level.hasAnyMPlatCollision(cx, cy - 1)
+      || level.hasAnyMPlatCollision(cx + M.round(xr), cy - 1)) {
+      // Set some squash for when you touch the ceiling
 
-			dashCount = 1;
-			jumpCount = 0;
+      // setSquashY(0.8);
+      dy = M.fabs(dy);
+    }
 
-			if (mPlat != null) {
-				mPlat.player = this;
-				this.plat = mPlat;
-			} else {
-				detachPlat();
-			}
-		}
-	}
+    // Down
+    if (level.hasAnyMPlatCollision(cx, cy + 1)
+      && yr >= 0. || level.hasAnyMPlatCollision(cx + M.round(xr), cy + 1)
+        && yr >= 0.5) {
+      // Handle squash and stretch for entities in the game
 
-	public function detachPlat() {
-		if (plat != null) {
-			plat.player = null;
-			plat.fixedUpdate();
-			plat = null;
-		}
-	}
+      var mPlat:MovingPlatform = cast level.collidedMPlat(cx, cy + 1);
+      isOnFloor = true;
+      canJump = true;
 
-	public function levelCollisions() {
-		// Left
-		if (level.hasAnyCollision(cx - 1, cy) && xr <= 0.3) {
-			xr = 0.3;
-			dx = 0;
-			// dx = M.fabs(dx);
-		}
+      dashCount = 1;
+      jumpCount = 0;
 
-		// Right
-		if (level.hasAnyCollision(cx + 1, cy) && xr >= 0.1) {
-			// push back to previous cell
-			xr = 0.1;
-			dx = 0;
-			// dx = (-1 * M.fabs(dx));
-		}
+      if (mPlat != null) {
+        mPlat.player = this;
+        this.plat = mPlat;
+      } else {
+        detachPlat();
+      }
+    }
+  }
 
-		// Up
-		if (level.hasAnyCollision(cx, cy - 1) || level.hasAnyCollision(cx + M.round(xr), cy - 1)) {
-			// Set some squash for when you touch the ceiling
-			setSquashY(0.8);
-			dy = M.fabs(dy);
-		}
+  public function detachPlat() {
+    if (plat != null) {
+      plat.player = null;
+      plat.fixedUpdate();
+      plat = null;
+    }
+  }
 
-		// Down
+  public function levelCollisions() {
+    // Left
+    if (level.hasAnyCollision(cx - 1, cy) && xr <= 0.3) {
+      xr = 0.3;
+      dx = 0;
+      // dx = M.fabs(dx);
+    }
 
-		if (level.hasAnyCollision(cx, cy + 1) && yr >= 0.5 || level.hasAnyCollision(cx + M.round(xr), cy + 1) && yr >= 0.5) {
-			// Handle squash and stretch for entities in the game
-			if (level.hasAnyCollision(cx, cy + M.round(yr + 0.3)) && !isOnFloor) {
-				setSquashY(0.6);
-			}
+    // Right
+    if (level.hasAnyCollision(cx + 1, cy) && xr >= 0.1) {
+      // push back to previous cell
+      xr = 0.1;
+      dx = 0;
+      // dx = (-1 * M.fabs(dx));
+    }
 
-			isOnFloor = true;
-			canJump = true;
+    // Up
+    if (level.hasAnyCollision(cx, cy - 1)
+      || level.hasAnyCollision(cx + M.round(xr), cy - 1)) {
+      // Set some squash for when you touch the ceiling
+      setSquashY(0.8);
+      dy = M.fabs(dy);
+    }
 
-			dashCount = 1;
-			jumpCount = 0;
-			// dy = 0;
+    // Down
 
-			// dy = (-1 * M.fabs(dy));
-			// We should be moved up by the moving platform by taking in the
-			// mPlatform velocity
-			dy = 0;
-			// If cy is still in object (yr)
-			yr = 0.5;
-		} else {
-			canJump = false;
-			isOnFloor = false;
-			if (plat == null) {
-				applyPhysics();
-			}
-		}
-	}
+    if (level.hasAnyCollision(cx, cy + 1)
+      && yr >= 0.5
+      || level.hasAnyCollision(cx + M.round(xr), cy + 1)
+      && yr >= 0.5) {
+      // Handle squash and stretch for entities in the game
+      if (level.hasAnyCollision(cx, cy + M.round(yr + 0.3)) && !isOnFloor) {
+        setSquashY(0.6);
+      }
 
-	public function applyPhysics() {
-		dy += 0.05;
-	}
+      isOnFloor = true;
+      canJump = true;
+
+      dashCount = 1;
+      jumpCount = 0;
+      // dy = 0;
+
+      // dy = (-1 * M.fabs(dy));
+      // We should be moved up by the moving platform by taking in the
+      // mPlatform velocity
+      dy = 0;
+      // If cy is still in object (yr)
+      yr = 0.5;
+    } else {
+      canJump = false;
+      isOnFloor = false;
+      if (plat == null) {
+        applyPhysics();
+      }
+    }
+  }
+
+  public function applyPhysics() {
+    dy += 0.05;
+  }
 }
--- src/en/enemy/Ziggle.hx
+++ src/en/enemy/Ziggle.hx
@@ -1,65 +1,65 @@
 package en.enemy;
 
 class Ziggle extends Enemy {
-	public var pathPoints:Array<tools.Vec2>;
-	public var looping:Bool;
-	public var pointIndex = 0;
-	public var speed:Float;
+  public var pathPoints:Array<tools.Vec2>;
+  public var looping:Bool;
+  public var pointIndex = 0;
+  public var speed:Float;
 
-	/**
-	 * Wait time between destinations per point.
-	 */
-	public var waitTime:Float;
+  /**
+   * Wait time between destinations per point.
+   */
+  public var waitTime:Float;
 
-	public function new(enemy:Entity_Enemy) {
-		super(enemy.cx, enemy.cy);
-		state = new State(idle);
-		looping = true;
-		speed = 0.05;
-		pathPoints = enemy.f_Path.map((pathPoint) -> {
-			return new Vec2(pathPoint.cx, pathPoint.cy);
-		});
-		// setSprite();
-	}
+  public function new(enemy:Entity_Enemy) {
+    super(enemy.cx, enemy.cy);
+    state = new State(idle);
+    looping = true;
+    speed = 0.05;
+    pathPoints = enemy.f_Path.map((pathPoint) -> {
+      return new Vec2(pathPoint.cx, pathPoint.cy);
+    });
+    // setSprite();
+  }
 
-	public function idle() {
-		followPath();
-		if (inLineOfSight()) {
-			state.currentState = attacking;
-		}
-	}
+  public function idle() {
+    followPath();
+    if (inLineOfSight()) {
+      state.currentState = attacking;
+    }
+  }
 
-	public function attacking() {
-		if (!inLineOfSight()) {
-			state.currentState = idle;
-		}
-		// Follow Player or move toward position
-		var dxNorm = M.fabs(Level.ME.hero.cx - cx);
-		dx = speed * (M.sign(dxNorm) * M.fabs(dxNorm / dxNorm));
-	}
+  public function attacking() {
+    if (!inLineOfSight()) {
+      state.currentState = idle;
+    }
+    // Follow Player or move toward position
+    var dxNorm = M.fabs(Level.ME.hero.cx - cx);
+    dx = speed * (M.sign(dxNorm) * M.fabs(dxNorm / dxNorm));
+  }
 
-	public function setSprite() {
-		var g = new h2d.Graphics(spr);
-		g.beginFill(0x101010, 1);
-		g.drawRect(0, 0, 16, 16);
-		g.endFill();
-	}
+  public function setSprite() {
+    var g = new h2d.Graphics(spr);
+    g.beginFill(0x101010, 1);
+    g.drawRect(0, 0, 16, 16);
+    g.endFill();
+  }
 
-	override public function update() {
-		super.update();
-		state.update();
-	}
+  override public function update() {
+    super.update();
+    state.update();
+  }
 
-	public function followPath() {
-		var point = pathPoints[pointIndex % pathPoints.length];
-		if (point.x != cx || point.y != cy) {
-			// Follow the path by checking the distance from point
-			var dest = new Vec2(point.x - cx, point.y - cy).normalize();
-			dx = dest.x * speed;
+  public function followPath() {
+    var point = pathPoints[pointIndex % pathPoints.length];
+    if (point.x != cx || point.y != cy) {
+      // Follow the path by checking the distance from point
+      var dest = new Vec2(point.x - cx, point.y - cy).normalize();
+      dx = dest.x * speed;
 
-			dy = dest.y * speed;
-		} else {
-			pointIndex++;
-		}
-	}
+      dy = dest.y * speed;
+    } else {
+      pointIndex++;
+    }
+  }
 }
